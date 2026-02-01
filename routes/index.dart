2e32a099b56c23d9453b46aa69a0400b3b2db6e0
AhmedShaltout85 <eng.abozaher@gmail.com>
2026-02-01 10:47:08 +0200

    Last-Update: 2026-02-01

--- routes/index.dart
+++ routes/index.dart
@@ -1,6 +1,6 @@
 import 'package:dart_frog/dart_frog.dart';
 
-Response onRequest(RequestContext context) {
+Future< Response> onRequest(RequestContext context) async{
   final request = context.read<String>();
   return Response(body: 'Welcome to Dart Frog ROOT route! $request');
 }
